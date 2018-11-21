776c23121717bed0dbe3782ab80b2e39265876c6
St1gven <proh_yar@mail.ru>
2018-11-21 17:38:35 +0400

    Return types set to void

--- src/main/java/com/mycompany/taskmanager/controller/Controller.java
+++ src/main/java/com/mycompany/taskmanager/controller/Controller.java
@@ -9,10 +9,10 @@ import java.time.temporal.TemporalAmount;
 public interface Controller {
 	setJournal(Journal journal);
 	
-	Task createTask(String name, String description, LocalDateTime datetime, NotificationType type);
+	void createTask(String name, String description, LocalDateTime datetime, NotificationType type);
 	void finishTask(int taskId);
-	Task postponeTask(int taskId, TemporalAmount shift);
-	Task postponeTask(int taskId, LocalDateTime datetime);
-	Task changeTask(String name, String description, LocalDateTime datetime,  NotificationType type);
+	void postponeTask(int taskId, TemporalAmount shift);
+	void postponeTask(int taskId, LocalDateTime datetime);
+	void changeTask(String name, String description, LocalDateTime datetime,  NotificationType type);
 	void cancelTask(int taskId);
 }
