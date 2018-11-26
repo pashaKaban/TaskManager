c9ef1084fad4ff854d089da26d0ea83ac31d08f9
St1gven <proh_yar@mail.ru>
2018-11-26 22:01:30 +0400

    Change stored property from task to taskId Add docs

--- src/main/java/com/mycompany/taskmanager/controller/Notification.java
+++ src/main/java/com/mycompany/taskmanager/controller/Notification.java
@@ -5,7 +5,20 @@ package com.mycompany.taskmanager.controller;
  * @author St1gven
  */
 public interface Notification {
-	Task getTask();
-	void setTask(Task task);
+	/**
+	 * Получить идентификатор задачи связанной с данным уведомлением
+	 * @return идентификатор задачи
+	 */
+	int getTaskId();
+	
+	/**
+	 * Установить задачу для данного уведомления
+	 * @param taskId идентификатор задачи
+	 */
+	void setTaskId(int taskId);
+	
+	/**
+	 * Уведомление пользователя о наступлении события
+	 */
 	void action();
 }
