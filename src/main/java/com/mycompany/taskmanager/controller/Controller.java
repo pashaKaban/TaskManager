393dc8bc0727251cbf05b3135445c3beab2dec66
St1gven <proh_yar@mail.ru>
2018-11-26 21:59:23 +0400

    Add notificationTime, update docs

--- src/main/java/com/mycompany/taskmanager/controller/Controller.java
+++ src/main/java/com/mycompany/taskmanager/controller/Controller.java
@@ -7,16 +7,20 @@ import java.time.temporal.TemporalAmount;
  * @author St1gven
  */
 public interface Controller {
+	/**
+	 * Устанавливает журнал с которым будет работать контроллер
+	 */
 	void setJournal(Journal journal);
 	
 	/**
 	 * Создание задачи, добавление ее в журнал
-	 * @param name Название задачи
-	 * @param description Описание задачи
-	 * @param datetime Время и дата задачи
-	 * @param type Тип уведомления
+	 * @param name название задачи
+	 * @param description описание задачи
+	 * @param datetime время и дата задачи
+	 * @param notificationTime время, в которое должно сработать оповещение
+	 * @param type тип уведомления
 	 */
-	void createTask(String name, String description, LocalDateTime datetime, NotificationType type);
+	void createTask(String name, String description, LocalDateTime datetime, LocalDateTime notificationTime, NotificationType type);
 	
 	/**
 	 * Изменить статус задачи на "завершенная"
@@ -31,7 +35,7 @@ public interface Controller {
 	 * @param shift количество времени, на которое перемещается задачу
 	 */
 	void postponeTask(int taskId, TemporalAmount shift);
-	
+
 	/**
 	 * Перенести задачу на определенное время
 	 * @param taskId идентификатор задачи
@@ -41,16 +45,18 @@ public interface Controller {
 	
 	/**
 	 * Изменить параметры задачи
+	 * @param taskId идентификатор задачи
 	 * @param name Название задачи
 	 * @param description Описание задачи
 	 * @param datetime Время и дата задачи
+	 * @param notificationTime время, в которое должно сработать оповещение
 	 * @param type Тип уведомления
 	 */
-	void changeTask(String name, String description, LocalDateTime datetime,  NotificationType type);
+	void changeTask(int taskId, String name, String description, LocalDateTime datetime, LocalDateTime notificationTime,  NotificationType type);
 	
 	/**
 	 * Изменить статус задачи на "отмененная"
-	 * @param taskId 
+	 * @param taskId идентификатор задачи
 	 */
 	void cancelTask(int taskId);
 }
