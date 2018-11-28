8053ba73d084d2694250cf1fd2acbaf6ffa5d387
alexKaban <36873459+pashaKaban@users.noreply.github.com>
2018-11-28 14:13:39 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/taskmanager/model/Task.java
@@ -0,0 +1,19 @@
+
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
+
+public interface Task {
+    String getId();
+    String getNameTask();
+    void setNameTask(String nameTask);
+    String getDiscription();
+    void setDiscription(String discription);
+    TemporalAmount getIntervalTime();
+    void setIntervalTime(TemporalAmount intervalTime);
+    LocalDateTime getPlanTime();
+    void setPlanTime(LocalDateTime planTime);
+    Status getStatus();
+    void setStatus(Status status);
+    NotificationType getNotificationType();
+    void setNotificationType(NotificationType notificationType);
+}
