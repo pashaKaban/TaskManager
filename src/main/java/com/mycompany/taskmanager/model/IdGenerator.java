0b3586ba23174ee177738d8566bf65c9a44405a3
alexKaban <36873459+pashaKaban@users.noreply.github.com>
2018-12-02 16:43:42 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/taskmanager/model/IdGenerator.java
@@ -0,0 +1,20 @@
+package com.mycompany.taskmanager.model;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class IdGenerator {
+    private static String identificator="";
+    private static final IdGenerator id=new IdGenerator();
+    private IdGenerator() {}
+    public static String generateId(){
+            synchronized (IdGenerator.class) {
+                //andNot(x) — операция побитовой дизъюнкции с дополнением this & (~x);
+                id.identificator=id.identificator.concat((String) new SimpleDateFormat("FDWddMMyyyyHHmmssSS").format(new Date())).concat((new BigInteger(128,10,new Random()).andNot(new BigInteger(128,10,new Random()))).toString());
+        }
+        return id.identificator;
+    }
+}
+
+
